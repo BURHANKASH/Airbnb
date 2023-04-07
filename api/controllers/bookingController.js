5d33475cf2a3b6217c0a4acf582783efac0e6b70
Burhan kash <108077620+BURHANKASH@users.noreply.github.com>
2023-04-07 19:56:05 +0530

    Add files via upload

---
+++ api/controllers/bookingController.js
@@ -0,0 +1,45 @@
+const Booking = require('../models/Booking');
+const userFromToken = require('../utils/userFromToken');
+
+exports.createBookings = async (req, res) => {
+  try {
+    const userData = userFromToken(req);
+    const { place, checkIn, checkOut, numOfGuests, name, phone, price } =
+      req.body;
+
+    const booking = await Booking.create({
+      user: userData.id,
+      place,
+      checkIn,
+      checkOut,
+      numOfGuests,
+      name,
+      phone,
+      price,
+    });
+
+    res.status(200).json({
+      booking,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: 'Internal server error',
+      error: err,
+    });
+  }
+};
+
+exports.getBookings = async (req, res) => {
+  try {
+    const userData = await userFromToken(req);
+    res
+      .status(200)
+      .json(await Booking.find({ user: userData.id }).populate('place'));
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Internal server error',
+      error: err,
+    });
+  }
+};
